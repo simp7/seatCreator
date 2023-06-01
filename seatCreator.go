6e00b21333faf05b497396ee25034bcbb92f3cac
lisa <twolovelove2232@gmail.com>
2023-06-01 14:12:44 +0900

    국립정동극장 sesil

--- seatCreator.go
+++ seatCreator.go
@@ -10,60 +10,51 @@ import (
 	"github.com/simp7/seatCreator/model/eraser"
 	"github.com/simp7/seatCreator/model/group"
 	"github.com/simp7/seatCreator/model/nameformatter"
+	"github.com/simp7/seatCreator/model/pos"
 )
 
 func ArtriumSmall() model.Group {
-	vertical := eraser.VerticalHallway(11,28)
 
-	nameFormatter := nameformatter.Standard()
-	integrated := eraser.Integrated(vertical)
+	rect := eraser.Rectangle(pos.Absolute{X: 8,Y: 12},pos.Absolute{X: 8,Y: 13})
+	integrated := eraser.Integrated(rect)
 
-	base := model.NewSeatBase(3, 5, "A석")
+	base := model.NewSeatBase(3, 4, "A석")
 	blockInput := group.BlockInput{
-		Criteria:      model.NewSeat(base, 1, 16),
-		XSize:         34,
-		YSize:         9,
+		Criteria:      model.NewSeat(base, 1, 1),
+		XSize:         6,
+		YSize:         11,
 		EmptyChecker:  integrated,
-		NameFormatter: nameFormatter,
+		NameFormatter: nameformatter.Prefix('A'),
 	}
 
-
-	base = model.NewSeatBase(6, 3, "A석")
-	blockInput2 := group.BlockInput{
-		Criteria:      model.NewSeat(base, 4, 14),
-		XSize:         28,
-		YSize:         2,
-		EmptyChecker:  integrated,
-		NameFormatter: nameFormatter,
+	rect2:=eraser.Rectangle(pos.Absolute{X: 10,Y: 12},pos.Absolute{X: 10,Y: 13})
+	rect3:=eraser.Rectangle(pos.Absolute{X: 19,Y: 12},pos.Absolute{X: 19,Y: 13})
+	integrated2 := eraser.Integrated(rect2,rect3)
+		base = model.NewSeatBase(10, 4, "A석")
+		blockInput2 := group.BlockInput{
+		Criteria:      model.NewSeat(base, 1, 1),
+		XSize:         10,
+		YSize:         11,
+		EmptyChecker:  integrated2,
+		NameFormatter: nameformatter.Prefix('B'),
 	}
 
-	base = model.NewSeatBase(3, 14, "A석")
-	blockInput3 := group.BlockInput{
-		Criteria:      model.NewSeat(base, 1, 25),
-		XSize:         4,
-		YSize:         1,
-		EmptyChecker:  integrated,
-		NameFormatter: nameFormatter,
+	rect4 := eraser.Rectangle(pos.Absolute{X: 21,Y: 12},pos.Absolute{X: 21,Y: 13})
+	integrated3 := eraser.Integrated(rect4)
+		base = model.NewSeatBase(21, 4, "A석")
+		blockInput3 := group.BlockInput{
+		Criteria:      model.NewSeat(base, 1, 1),
+		XSize:         6,
+		YSize:         11,
+		EmptyChecker:  integrated3,
+		NameFormatter: nameformatter.Prefix('C'),
 	}
-
-
-	base = model.NewSeatBase(13, 14, "A석")
-	blockInput4 := group.BlockInput{
-		Criteria:      model.NewSeat(base, 10, 25),
-		XSize:         24,
-		YSize:         1,
-		EmptyChecker:  integrated,
-		NameFormatter: nameFormatter,
-	}
-
 
 	block1 := group.HorizontalBlock(blockInput)
 	block2 := group.HorizontalBlock(blockInput2)
 	block3 := group.HorizontalBlock(blockInput3)
-	block4 := group.HorizontalBlock(blockInput4)
-	return group.Mixed(block1, block2, block3, block4)
+	return group.Mixed(block1, block2, block3)
 }
-
 
 func copy(target model.Group) {
 	err := clipboard.WriteAll(target.String())
@@ -76,14 +67,14 @@ func copy(target model.Group) {
 func handler(c *gin.Context) {
 	seats := ArtriumSmall() // Put Seating Here
 	target := area.Area{
-		Key:             "2F",
+		Key:             "1F",
 		Seats:           seats,
-		XSize:           34,
-		YSize:           12,
+		XSize:           24,
+		YSize:           11,
 		BackgroundImage: "",
 		Color:           "#ff9f00",
 	}
-  
+
 	fmt.Fprintf(c.Writer, "<h1>Seats Map</h1><body>\n%s\n</body>", target.Html())
 	copy(target)
 }
