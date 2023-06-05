5a735b206ea35f7a81451a9552aef18020b3fc17
lisa <twolovelove2232@gmail.com>
2023-06-05 09:59:02 +0900

    평촌 아트홀 2층

--- seatCreator.go
+++ seatCreator.go
@@ -13,55 +13,72 @@ import (
 	"github.com/simp7/seatCreator/model/pos"
 )
 
-func ConcertHall1F() model.Group {	
+func ConcertHall1F() model.Group {
 	nameFormatter := nameformatter.Standard()
 	
-	hall := eraser.VerticalHallway(11, 29)
-	hall2 := eraser.HorizontalHallway(15)
-	integrated := eraser.Integrated(hall, hall2)
-	base := model.NewSeatBase(3, 4, "A석")
+	integrated := eraser.Integrated()
+	base := model.NewSeatBase(4, 4, "가석")
 	blockInput := group.BlockInput{
-		Criteria:      model.NewSeat(base, 1, 2),
-		XSize:         35,
-		YSize:         17,
-		EmptyChecker:  integrated,
-		NameFormatter: nameFormatter,
-	}
-
-	base = model.NewSeatBase(4, 3, "A석")
-	blockInput2 := group.BlockInput{
-		Criteria:      model.NewSeat(base, 2, 1),
-		XSize:         33,
-		YSize:         1,
-		EmptyChecker:  integrated,
-		NameFormatter: nameFormatter,
-	}
-
-	base = model.NewSeatBase(34, 21, "A석")
-	blockInput3 := group.BlockInput{
-		Criteria:      model.NewSeat(base, 30, 18),
-		XSize:         4,
-		YSize:         1,
-		EmptyChecker:  integrated,
-		NameFormatter: nameFormatter,
-	}
-
-	rect3 := eraser.Rectangle(pos.Absolute{X:8,Y: 21 },pos.Absolute{X: 29, Y: 21})
-	integrated2 := eraser.Integrated(rect3)
-	base = model.NewSeatBase(4, 21, "휠체어석")
-	blockInput4 := group.BlockInput{
 		Criteria:      model.NewSeat(base, 1, 1),
-		XSize:         28,
-		YSize:         1,
-		EmptyChecker:  integrated2,
-		NameFormatter: nameformatter.Prefix('W'),
+		XSize:         1,
+		YSize:         8,
+		EmptyChecker:  integrated,
+		NameFormatter: nameFormatter,
+	}
+	base = model.NewSeatBase(3, 4, "가석")
+	blockInput2 := group.BlockInput{
+		Criteria:      model.NewSeat(base, 9, 1),
+		XSize:         1,
+		YSize:         9,
+		EmptyChecker:  integrated,
+		NameFormatter: nameFormatter,
 	}
 
-	block1 := group.HorizontalBlock(blockInput)
-	block2 := group.HorizontalBlock(blockInput2)
-	block3 := group.HorizontalBlock(blockInput3)
-	block4 := group.HorizontalBlock(blockInput4)
-	return group.Mixed(block1, block2, block3, block4)
+	base = model.NewSeatBase(4, 14, "나석")
+	blockInput3 := group.BlockInput{
+		Criteria:      model.NewSeat(base, 1, 1),
+		XSize:         1,
+		YSize:         8,
+		EmptyChecker:  integrated,
+		NameFormatter: nameFormatter,
+	}
+	base = model.NewSeatBase(3, 14, "나석")
+	blockInput4 := group.BlockInput{
+		Criteria:      model.NewSeat(base, 9, 1),
+		XSize:         1,
+		YSize:         9,
+		EmptyChecker:  integrated,
+		NameFormatter: nameFormatter,
+	}
+
+	specific := eraser.Position(pos.Absolute{X: 35, Y: 12}, pos.Absolute{X: 35,Y: 22})
+	integrated2 := eraser.Integrated(specific)
+	base = model.NewSeatBase(35, 4, "다석")
+	blockInput5 := group.BlockInput{
+		Criteria:      model.NewSeat(base, 1, 1),
+		XSize:         2,
+		YSize:         9,
+		EmptyChecker:  integrated2,
+		NameFormatter: nameformatter.Prefix('A'),
+	}
+
+	base = model.NewSeatBase(35, 14, "라석")
+	blockInput6 := group.BlockInput{
+		Criteria:      model.NewSeat(base, 1, 1),
+		XSize:         2,
+		YSize:         9,
+		EmptyChecker:  integrated2,
+		NameFormatter: nameformatter.Prefix('A'),
+	}
+	
+
+	block1 := group.VerticalBlock(blockInput)
+	block2 := group.VerticalBlock(blockInput2)
+	block3 := group.VerticalBlock(blockInput3)
+	block4 := group.VerticalBlock(blockInput4)
+	block5 := group.VerticalBlock(blockInput5)
+	block6 := group.VerticalBlock(blockInput6)
+	return group.Mixed(block1, block2, block3, block4, block5, block6)
 }
 
 
@@ -77,15 +94,14 @@ func copy(target model.Group) {
 func handler(c *gin.Context) {
 	seats := ConcertHall1F() // Put Seating Here
 	target := area.Area{
-		Key:             "1F",
+		Key:             "2F",
 		Seats:           seats,
-		XSize:           35,
-		YSize:           19,
+		XSize:           36,
+		YSize:           35,
 		BackgroundImage: "",
 		Color:           "#ff9f00",
 	}
 
-  
 	fmt.Fprintf(c.Writer, "<h1>Seats Map</h1><body>\n%s\n</body>", target.Html())
 	copy(target)
 }
