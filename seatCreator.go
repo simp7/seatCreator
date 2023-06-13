ac809095c907949bb08f551dbf5db52fb9b44beb
lisa <twolovelove2232@gmail.com>
2023-06-13 09:41:36 +0900

    정극장

--- seatCreator.go
+++ seatCreator.go
@@ -16,22 +16,17 @@ import (
 func ArtriumSmall() model.Group {
 	nameFormatter := nameformatter.Standard()
 	
-	rect1 := eraser.Rectangle(pos.Absolute{X: 11, Y: 4}, pos.Absolute{X: 12,Y: 13})
-	rect2 := eraser.Rectangle(pos.Absolute{X: 13, Y: 4}, pos.Absolute{X: 20, Y: 4})
-	rect3 := eraser.Rectangle(pos.Absolute{X: 18, Y: 5}, pos.Absolute{X: 20, Y: 5})
-	rect4 := eraser.Rectangle(pos.Absolute{X: 16, Y: 13}, pos.Absolute{X: 20, Y: 14})
+	rect := eraser.Rectangle(pos.Absolute{X: 13, Y: 4}, pos.Absolute{X: 14, Y: 8})
+	specific := eraser.Position(pos.Absolute{X: 12,Y: 9}, pos.Absolute{X: 17, Y: 9})
 
-	specific := eraser.Position(pos.Absolute{X: 3, Y: 5}, pos.Absolute{X: 20, Y: 12})
-
-	integrated := eraser.Integrated(rect1, rect2, rect3, rect4, specific)
+	integrated := eraser.Integrated(rect, specific)
 	base := model.NewSeatBase(3, 4, "A석")
 	blockInput := group.BlockInput{
 		Criteria:      model.NewSeat(base, 1, 1),
-		XSize:         18,
-		YSize:         11,
+		XSize:         22,
+		YSize:         6,
 		EmptyChecker:  integrated,
 		NameFormatter: nameFormatter,
-		Reverse:	   true,	
 	}
 	
 	block1 := group.HorizontalBlock(blockInput)
@@ -53,8 +48,8 @@ func handler(c *gin.Context) {
 	target := area.Area{
 		Key:             "1F",
 		Seats:           seats,
-		XSize:           18,
-		YSize:           11,
+		XSize:           22,
+		YSize:           6,
 		BackgroundImage: "",
 		Color:           "#ff9f00",
 	}
